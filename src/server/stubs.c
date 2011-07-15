ffe9157866666dd8e4fa8a7d92f0d1d490720feb
chambart <chambart@crans.org>
2011-07-15 17:49:14 +0200

    get rid of C stubs
    Ignore-this: 86c1be685059437543398094dcc22912
    
    darcs-hash:20110715154914-0cb73-1c8048b7d9f0057acbd06995c0b79b61bc6d9cc2

--- src/server/stubs.c
+++
@@ -1,9 +0,0 @@
-#include <caml/memory.h>
-#include <caml/mlvalues.h>
-#include <caml/fail.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netinet/tcp.h>
-#include <grp.h>
-
