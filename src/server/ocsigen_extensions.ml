33816a0e2d3b6886c25d1968b5e282490be927e3
Romain Calascibetta <romain.calascibetta@gmail.com>
2014-04-07 17:24:12 +0200

    Move handle of command in Ocsigen_command (and retransmits its signature to Ocsigen_extensions)

--- src/server/ocsigen_extensions.ml
+++ src/server/ocsigen_extensions.ml
@@ -37,6 +37,8 @@ open Lwt
 open Ocsigen_lib
 open Ocsigen_cookies
 
+include Ocsigen_command
+
 module Ocsigen_request_info = Ocsigen_request_info
 
 exception Ocsigen_http_error of (Ocsigen_cookies.cookieset * int)
@@ -1144,33 +1146,6 @@ let find_redirection regexp full_url dest
       | Some g -> sub_path_string ^ "?" ^ g
     in
     match Netstring_pcre.string_match regexp path 0 with
-      | None -> raise Not_concerned
-      | Some _ -> (* Matching regexp found! *)
-          Netstring_pcre.global_replace regexp dest path
-
-
-(******************************************************************)
-(* Extending commands *)
-exception Unknown_command
-
-let register_command_function, get_command_function =
-  let command_function = ref (fun ?prefix _ _ -> Lwt.fail Unknown_command) in
-  ((fun ?prefix f ->
-      let prefix' = prefix in
-      let old_command_function = !command_function in
-      command_function :=
-        (fun ?prefix s c ->
-           Lwt.catch (fun () -> old_command_function ?prefix s c)
-             (function
-               | Unknown_command ->
-                 if prefix = prefix'
-                 then f s c
-                 else Lwt.fail Unknown_command
-               | e -> Lwt.fail e))),
-   (fun () -> !command_function))
-
-
-let () =
-  register_command_function
-    ~prefix:"logs"
-    (Ocsigen_messages.command_f Unknown_command)
+    | None -> raise Not_concerned
+    | Some _ -> (* Matching regexp found! *)
+        Netstring_pcre.global_replace regexp dest path
