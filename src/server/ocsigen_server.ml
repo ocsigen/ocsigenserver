830dededbadc74246a6322dac0e9b1f23e421d39
Romain Calascibetta <romain.calascibetta@gmail.com>
2014-04-07 16:03:47 +0200

    Move socket_type in another module for separate dependence between Ocsigen_server and Ocsigen_parseconfig

--- src/server/ocsigen_server.ml
+++ src/server/ocsigen_server.ml
@@ -20,6 +20,7 @@
 
 open Lwt
 open Ocsigen_messages
+open Ocsigen_socket
 open Ocsigen_lib
 open Ocsigen_extensions
 open Ocsigen_http_frame
@@ -54,41 +55,6 @@ let _ =
     (fun e -> Ocsigen_messages.errlog ("Uncaught Exception after lwt timeout: "^
                                  Printexc.to_string e))
 
-let make_ipv6_socket addr port =
-  let socket = Lwt_unix.socket Unix.PF_INET6 Unix.SOCK_STREAM 0 in
-  Lwt_unix.set_close_on_exec socket;
-  Lwt_unix.setsockopt socket Unix.SO_REUSEADDR true;
-  Lwt_unix.setsockopt socket Unix.IPV6_ONLY true;
-  Lwt_unix.bind socket (Unix.ADDR_INET (addr, port));
-  socket
-
-let make_ipv4_socket addr port =
-  let socket = Lwt_unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
-  Lwt_unix.set_close_on_exec socket;
-  Lwt_unix.setsockopt socket Unix.SO_REUSEADDR true;
-  Lwt_unix.bind socket (Unix.ADDR_INET (addr, port));
-  socket
-
-let make_sockets addr port =
-  match addr with
-    | All ->
-        (* The user didn't specify a protocol in the configuration
-           file; we try to open an IPv6 socket (listening to IPv6
-           only) if possible and we open an IPv4 socket anyway. This
-           corresponds to the net.ipv6.bindv6only=0 behaviour on Linux,
-           but is portable and should work with
-           net.ipv6.bindv6only=1 as well. *)
-      let ipv6_socket =
-        try [make_ipv6_socket Unix.inet6_addr_any port]
-        with Unix.Unix_error
-            ((Unix.EAFNOSUPPORT | Unix.EPROTONOSUPPORT),
-             _, _) -> []
-      in
-      (make_ipv4_socket Unix.inet_addr_any port)::ipv6_socket
-    | IPv4 addr ->
-      [make_ipv4_socket addr port]
-    | IPv6 addr ->
-      [make_ipv6_socket addr port]
 
 let sslctx = Ocsigen_http_client.sslcontext
 
