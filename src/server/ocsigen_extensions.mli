b36e05becb28e54caee4136b6e284cc616b7293e
Romain Calascibetta <romain.calascibetta@gmail.com>
2014-04-07 17:24:12 +0200

    Move handle of command in Ocsigen_command (and retransmits its signature to Ocsigen_extensions)

--- src/server/ocsigen_extensions.mli
+++ src/server/ocsigen_extensions.mli
@@ -30,6 +30,8 @@ open Lwt
 open Ocsigen_lib
 open Ocsigen_cookies
 
+include (module type of Ocsigen_command)
+
 module Ocsigen_request_info : (module type of Ocsigen_request_info
                                 with type request_info = Ocsigen_request_info.request_info
                                  and type file_info = Ocsigen_request_info.file_info
@@ -475,26 +477,6 @@ val find_redirection :
   bool ->
   string option -> int -> string option -> string -> string -> string
 
-
-(** {3 Extending server commands} *)
-exception Unknown_command
-
-(** Use a prefix for all your commands when you want to create
-    extension-specific commands.
-    For example if the prefix is "myextension" and the commande "blah",
-    the actual command to be written by the user is "myextension:blah".
-    Give as parameter the function that will parse the command and do an action.
-    Its first parameter is the full command as a string.
-    The second one is the command without prefix, split by word.
-    It must raise [ocsigen_extensions.Unknown_command] if it does
-    not recognize the command.
-*)
-val register_command_function :
-  ?prefix:string -> (string -> string list -> unit Lwt.t) -> unit
-
-(**/**)
-val get_command_function :
-  unit -> (?prefix:string -> string -> string list -> unit Lwt.t)
 
 (**/**)
 (**/**)
