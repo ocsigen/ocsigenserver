f74768adc3357ab710ea6d5fa928c7bd5216b3d1
Romain Calascibetta <romain.calascibetta@gmail.com>
2014-04-09 17:46:14 +0200

    Move all of HTTP handle in Ocsigen_brouette, Ocsigen_server is now loop server and nothing other

--- src/server/ocsigen_brouette.mli
+++ src/server/ocsigen_brouette.mli
@@ -2,3 +2,22 @@
  * Ocsigen_extensions and Ocsigen_server *)
 
 exception Ocsigen_Is_a_directory of (Ocsigen_request_info.request_info -> Neturl.url)
+exception Ocsigen_unsupported_media
+exception Ocsigen_upload_forbidden
+
+val sockets : Lwt_unix.file_descr list ref
+val sslsockets : Lwt_unix.file_descr list ref
+val sslctx : Ssl.context ref
+
+val stop : string -> int -> 'a
+
+val get_number_of_connected : unit -> int
+
+val shutdown_server : string -> string list -> unit
+
+val listen :
+  bool ->
+  Ocsigen_socket.socket_type * int ->
+  unit Lwt.t ->
+  (Ocsigen_request_info.request_info -> unit -> Ocsigen_http_frame.result Lwt.t) ->
+  Lwt_unix.file_descr list
