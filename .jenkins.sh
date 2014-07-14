bd7cd95aa9bb914c0ee025a523c02250691f1dac
Grégoire Henry <gregoire.henry@ocamlpro.com>
2014-07-14 18:06:41 +0200

    Jenkins: build_doc.

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,5 +1,17 @@
+
 opam pin add --no-action ocsigenserver .
 opam install camlzip
 opam install --deps-only ocsigenserver
 opam install --verbose ocsigenserver
-opam remove --verbose ocsigenserver
+
+do_build_doc () {
+  make -C doc clean
+  make -C doc doc
+  make -C doc wikidoc
+  cp -rf doc/api-wiki/*.wiki ${API_DIR}
+  cp -rf doc/api-wiki/*.wiki ${MANUAL_SRC_DIR}
+}
+
+do_remove () {
+  opam remove --verbose ocsigenserver
+}
