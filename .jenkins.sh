f51d6cf2314f4197c6d259d8226710326dae3b18
Romain Calascibetta <romain.calascibetta@gmail.com>
2014-08-04 18:28:42 +0200

    Add new compilation in jenkins

--- .jenkins.sh
+++ .jenkins.sh
@@ -3,6 +3,10 @@ opam install camlzip
 opam install --deps-only ocsigenserver
 opam install --verbose ocsigenserver
 
+./configure
+make all
+make -C src/sandbox/ all
+
 do_build_doc () {
   make -C doc clean
   make -C doc doc
