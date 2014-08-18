e68623def120c02a38f3e5fa2ab38cb352bd920f
Romain Calascibetta <romain.calascibetta@gmail.com>
2014-08-18 14:28:41 +0200

    Improve documentation

--- src/migrate/of_cohttp.mli
+++ src/migrate/of_cohttp.mli
@@ -1,6 +1,10 @@
+(** Module to cast Cohttp value to OcsigenServer value *)
+
+(** [of_version] cast version of protocol *)
 val of_version :
   [> `HTTP_1_0 | `HTTP_1_1 ] -> Ocsigen_http_frame.Http_header.proto
 
+(** [of_meth] cast method of request *)
 val of_meth :
   [< `DELETE
    | `GET
@@ -12,23 +16,32 @@ val of_meth :
    | `PUT ] ->
   Ocsigen_http_frame.Http_header.http_method
 
+(** [of_headers] cast Cohttp headers to OcsigenServer headers (this function is
+    [fun x -> x] with simply annotation) *)
 val of_headers : Cohttp.Header.t -> Http_headers.t
 
+(** [of_request] extracts only header of Cohttp request *)
 val of_request :
   Cohttp.Request.t -> Ocsigen_http_frame.Http_header.http_header
 
+(** [of_response] extracts only header of Cohttp response *)
 val of_response :
   Cohttp.Response.t -> Ocsigen_http_frame.Http_header.http_header
 
+(** [of_request_and_body] cast Cohttp request to OcsigenServer request *)
 val of_request_and_body :
   Cohttp.Request.t * Cohttp_lwt_body.t -> Ocsigen_http_frame.t
 
+(** [of_date] cast a date (as [string]) to timestamp *)
 val of_date : string -> float
 
 val of_charset : string -> string option
 
+(** [of_response_and_body] cast Cohttp response to OcsigenServer response *)
 val of_response_and_body :
   Cohttp.Response.t * Cohttp_lwt_body.t -> Ocsigen_http_frame.result
 
+(** [of_response_and_body'] cast Cohttp response to OcsigenServer frame (like a
+    request). It's specially used by [revproxy] extension *)
 val of_response_and_body' :
   Cohttp.Response.t * Cohttp_lwt_body.t -> Ocsigen_http_frame.t
--- src/migrate/to_cohttp.mli
+++ src/migrate/to_cohttp.mli
@@ -1,6 +1,18 @@
+(** Module to cast OcsigenServer value to Cohttp value *)
+
+(** Module to serialize cookie to Cohttp headers *)
+module Cookie :
+  sig
+    val serialize :
+      Ocsigen_cookies.cookie Ocsigen_cookies.CookiesTable.t
+      Ocsigen_cookies.Cookies.t -> Cohttp.Header.t -> Cohttp.Header.t
+  end
+
+(** [to_version] cast version of protocol *)
 val to_version :
   Ocsigen_http_frame.Http_header.proto -> [> `HTTP_1_0 | `HTTP_1_1 ]
 
+(** [to_meth] cast method of request *)
 val to_meth :
   Ocsigen_http_frame.Http_header.http_method ->
   [> `DELETE
@@ -12,26 +24,33 @@ val to_meth :
    | `POST
    | `PUT ]
 
+(** [to_headers] cast OcsigenServer headers to Cohttp headers (this function is
+    [fun x -> x] with simply annotation) *)
 val to_headers : Http_headers.t -> Cohttp.Header.t
 
+(** [to_response] injects only header of new Cohttp response *)
 val to_response :
   ?encoding:Cohttp.Transfer.encoding ->
   ?flush:bool ->
   Ocsigen_http_frame.Http_header.http_header -> Cohttp.Response.t
 
+(** [to_request] injects only headers to new Cohttp request *)
 val to_request :
   ?encoding:Cohttp.Transfer.encoding ->
   Ocsigen_http_frame.Http_header.http_header -> Uri.t -> Cohttp.Request.t
 
+(** [to_request_and_body] cast a OcsigenServer request to Cohttp request *)
 val to_request_and_body :
   ?encoding:Cohttp.Transfer.encoding ->
   Ocsigen_http_frame.t ->
   Uri.t -> Cohttp.Request.t * [> `Stream of string Lwt_stream.t ]
 
+(** [to_date] cast a date (as timestamp) to a string *)
 val to_date : float -> string
 
 val to_type : string -> string option -> string
 
+(** [to_response_and_body] cast a OcsigenServer response to Cohttp response *)
 val to_response_and_body :
   Ocsigen_http_frame.result ->
   Cohttp.Response.t * [> `Stream of string Lwt_stream.t ]
