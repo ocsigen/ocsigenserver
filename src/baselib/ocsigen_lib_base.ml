8eebe1349497a9fe48ef3432350370b82a727575
Drup <drupyog@zoho.com>
2014-11-28 03:09:17 +0100

    Utf8-ise a file.

--- src/baselib/ocsigen_lib_base.ml
+++ src/baselib/ocsigen_lib_base.ml
@@ -1,5 +1,5 @@
 (* Ocsigen
- * Copyright (C) 2005-2008 Vincent Balat, Stéphane Glondu
+ * Copyright (C) 2005-2008 Vincent Balat, StÃ©phane Glondu
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
