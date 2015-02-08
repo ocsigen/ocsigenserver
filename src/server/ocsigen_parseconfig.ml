5142d03798b3bf40e0fedce29b1e2c2be2d583d9
Rudi Grinberg <rudi.grinberg@gmail.com>
2015-02-08 00:41:34 -0500

    the saga continues

--- src/server/ocsigen_parseconfig.ml
+++ src/server/ocsigen_parseconfig.ml
@@ -1,7 +1,7 @@
 (* Ocsigen
  * http://www.ocsigen.org
  * Module ocsigen_parseconfig.ml
- * Copyright (C) 2005-2008 Vincent Balat, Nataliya Guts, Stéphane Glondu
+ * Copyright (C) 2005-2008 Vincent Balat, Nataliya Guts, StÃ©phane Glondu
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
