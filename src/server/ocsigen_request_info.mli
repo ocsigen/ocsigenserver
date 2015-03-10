bf13f7aa104ae2a87692f26c961b6ed721a7c290
Jerome Vouillon <jerome.vouillon@pps.univ-paris-diderot.fr>
2015-03-10 15:21:38 +0100

    Move ri_of_url to Ocsigen_request_info

--- src/server/ocsigen_request_info.mli
+++ src/server/ocsigen_request_info.mli
@@ -12,6 +12,12 @@ type file_info = {
 
 type request_info
 
+(** Parsing URLs.
+   This allows to modify the URL in the request_info.
+   (to be used for example with Ext_retry_with or Ext_continue_with)
+ *)
+val ri_of_url : ?full_rewrite:bool -> string -> request_info -> request_info
+
 val make :
   url_string:string ->
   meth:Ocsigen_http_frame.Http_header.http_method ->
