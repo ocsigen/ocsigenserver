c8d6c31e97718f9e2d992cbb9fb40f5a60737ef8
Jerome Vouillon <jerome.vouillon@pps.univ-paris-diderot.fr>
2015-07-07 16:25:15 +0200

    Corrections in Ocsigen_http_client

--- src/server/ocsigen_http_client.mli
+++ src/server/ocsigen_http_client.mli
@@ -7,7 +7,6 @@
     {!Ocsigen_stream.finalize}, otherwise you will have fd leaks.
 *)
 val get :
-    ?v6:bool ->
     ?https: bool ->
     ?port:int ->
     ?headers: Http_headers.t ->
@@ -23,7 +22,6 @@ val get :
     {!Ocsigen_stream.finalize}, otherwise you will have fd leaks.
 *)
 val post_string :
-  ?v6:bool ->
   ?https: bool ->
   ?port:int ->
   ?headers: Http_headers.t ->
@@ -41,7 +39,6 @@ val post_string :
     {!Ocsigen_stream.finalize}, otherwise you will have fd leaks.
 *)
 val post_urlencoded :
-  ?v6:bool ->
   ?https: bool ->
   ?port:int ->
   ?headers: Http_headers.t ->
@@ -50,3 +47,50 @@ val post_urlencoded :
   content:(string * string) list ->
   unit ->
   Ocsigen_http_frame.t Lwt.t
+
+val raw_request :
+  ?keep_alive: bool ->
+  ?headers: Http_headers.t ->
+  ?https: bool ->
+  ?port:int ->
+  content: string Ocsigen_stream.t option ->
+  ?content_length: int64 ->
+  http_method: Ocsigen_http_frame.Http_header.http_method ->
+  host:string ->
+  inet_addr:Unix.inet_addr ->
+  uri:string ->
+  unit ->
+  unit ->
+  Ocsigen_http_frame.t Lwt.t
+(**
+   Do an HTTP request (low level).
+
+   If the optional argument [headers] is present, no headers will be
+   added by Ocsigen, but those in this argument and host, and
+   [connection: close] or [connection: keep-alive].
+   Be carefull to respect HTTP/1.1 in this case!
+   ([host] is the full Host HTTP field to send).
+
+   The default port is 80 for HTTP, 443 for HTTPS.
+
+   The default protocol is http ([https=false]).
+
+   The parameters [?keep_alive] and [~inet_addr] are ignored.
+*)
+
+val basic_raw_request :
+  ?headers: Http_headers.t ->
+  ?https: bool ->
+  ?port:int ->
+  content: string Ocsigen_stream.t option ->
+  ?content_length: int64 ->
+  http_method: Ocsigen_http_frame.Http_header.http_method ->
+  host:string ->
+  inet_addr:Unix.inet_addr ->
+  uri:string ->
+  unit ->
+  Ocsigen_http_frame.t Lwt.t
+(** Same as {!Ocsigen_http_client.raw_request},
+    but does not try to reuse connections.
+    Opens a new connections for each request. Far less efficient.
+*)
