b3932c76cf97e92a0546355c23ea69f0451f37ec
Jerome Vouillon <jerome.vouillon@pps.univ-paris-diderot.fr>
2015-07-07 16:25:15 +0200

    Corrections in Ocsigen_http_client

--- src/server/ocsigen_http_client.ml
+++ src/server/ocsigen_http_client.ml
@@ -3,10 +3,12 @@ open Ocsigen_lib
 open Cohttp
 open Cohttp_lwt_unix
 
-let post_string ?v6 ?(https = false) ?port ?(headers = Http_headers.empty)
+let target https host ?port uri =
+  let scheme = if https then "https" else "http" in
+  Uri.resolve scheme (Uri.make ~scheme ~host ?port ()) (Uri.of_string uri)
+
+let post_string ?(https = false) ?port ?(headers = Http_headers.empty)
     ~host ~uri ~content ~content_type () =
-  Ip_address.get_inet_addr ?v6 host >>= fun inet_addr ->
-  let host = Unix.string_of_inet_addr inet_addr in
   let content_type = String.concat "/" [fst content_type; snd content_type] in
   let ( |> ) a f = f a in
   let headers =
@@ -15,28 +17,49 @@ let post_string ?v6 ?(https = false) ?port ?(headers = Http_headers.empty)
     |> Http_headers.add Http_headers.content_length
       (string_of_int (String.length content))
     |> To_cohttp.to_headers in
-  let scheme = if https then "https" else "http" in
-  let target = Uri.make ~scheme ~host ?port ~path:uri () in
   Cohttp_lwt_unix.Client.post
     ~body:(Cohttp_lwt_body.of_string content)
     ~headers
-    target
+    (target https host ?port uri)
   >|= Of_cohttp.of_response_and_body'
 
-(* XXX: headers is unused at top of ocsigenserver,
- * but cast with cohttp headers is necessary *)
-
-let get ?v6 ?(https = false) ?port ?headers ~host ~uri () =
-  Ip_address.get_inet_addr ?v6 host >>= fun inet_addr ->
-  let host = Unix.string_of_inet_addr inet_addr in
-  let scheme = if https then "https" else "http" in
-  let target = Uri.make ~scheme ~host ?port ~path:uri () in
-  Cohttp_lwt_unix.Client.get ?headers target
+let get ?(https = false) ?port ?headers ~host ~uri () =
+  Cohttp_lwt_unix.Client.get ?headers (target https host ?port uri)
   >|= Of_cohttp.of_response_and_body'
 
-let post_urlencoded ?v6 ?https ?port ?headers ~host ~uri ~content () =
-  post_string ?v6 ?https ?port ?headers
+let post_urlencoded ?https ?port ?headers ~host ~uri ~content () =
+  post_string ?https ?port ?headers
     ~host ~uri
     ~content:(Netencoding.Url.mk_url_encoded_parameters content)
     ~content_type:("application","x-www-form-urlencoded")
     ()
+
+let basic_raw_request
+    ?(headers = Http_headers.empty) ?(https=false) ?port
+    ~content ?content_length
+    ~http_method ~host ~inet_addr ~uri () =
+  ignore inet_addr;
+  let headers =
+    match content_length with
+      Some len -> Http_headers.add Http_headers.content_length
+                    (Int64.to_string len) headers
+    | None     -> headers
+  in
+  let body =
+    match content with
+      Some c ->
+        Some (Cohttp_lwt_body.of_stream (Ocsigen_stream.to_lwt_stream c))
+    | None ->
+        None
+  in
+  Cohttp_lwt_unix.Client.call ~headers ?body
+    (To_cohttp.to_meth http_method) (target https host ?port uri)
+  >|= Of_cohttp.of_response_and_body'
+
+let raw_request
+    ?keep_alive ?headers ?https ?port
+    ~content ?content_length ~http_method ~host ~inet_addr ~uri () () =
+  ignore keep_alive;
+  basic_raw_request
+    ?headers ?https ?port ~content ?content_length
+    ~http_method ~host ~inet_addr ~uri ()
