556a155376669aba34f1e93a88a9b308107af8be
Vasilis Papavasileiou <git@vasilis.airpost.net>
2016-04-11 12:31:21 +0000

    Catch up with Tyxml
    - Html5.M -> Tyxml.Html
    - Bump-up dependency constraint

--- src/http/ocsigen_senders.mli
+++ src/http/ocsigen_senders.mli
@@ -23,8 +23,8 @@ module File_content : Ocsigen_http_frame.HTTP_CONTENT
   with  type t =
     string * Ocsigen_charset_mime.charset_assoc * Ocsigen_charset_mime.mime_assoc
 
-module Html5_content :
-  Ocsigen_http_frame.HTTP_CONTENT with type t = Html5.M.doc
+module Html_content :
+  Ocsigen_http_frame.HTTP_CONTENT with type t = Tyxml.Html.doc
 
 module Make_XML_Content(Xml : Xml_sigs.Iterable)
     (Typed_xml : Xml_sigs.Typed_xml with module Xml := Xml) :
