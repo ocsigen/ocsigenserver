2c61c8aef5a5904fc5658d07b89c058690753233
Jan Rochel <jrochel@besport.com>
2016-07-08 17:10:28 +0200

    don't use unnecessary transaction blocks

--- src/extensions/ocsipersist-pgsql/ocsipersist.ml
+++ src/extensions/ocsipersist-pgsql/ocsipersist.ml
@@ -28,22 +28,10 @@ let connect () = PGOCaml.connect
 
 let (>>) f g = f >>= fun _ -> g
 
-let transaction_block db f =
-  PGOCaml.begin_work db >>= fun _ ->
-  try_lwt
-    lwt r = f () in
-    PGOCaml.commit db >>
-    Lwt.return r
-  with e ->
-    print_endline (Printexc.to_string e);
-    PGOCaml.rollback db >>
-    Lwt.fail e
-
 let pool : (string, bool) Hashtbl.t PGOCaml.t Lwt_pool.t =
   Lwt_pool.create 16 ~validate:PGOCaml.alive connect
 
-let full_transaction_block f = (* copied from Eba_db *)
-  Lwt_pool.use pool (fun db -> transaction_block db (fun () -> f db))
+let use_pool f = Lwt_pool.use pool (fun db -> f db)
 
 let exec db query params =
   PGOCaml.prepare db ~query () >>
@@ -81,10 +69,10 @@ type 'a t = {
   name  : string;
 }
 
-let open_store store = full_transaction_block @@ fun db ->
+let open_store store = use_pool @@ fun db ->
   create_table db store >> Lwt.return store
 
-let make_persistent_lazy_lwt ~store ~name ~default = full_transaction_block @@ fun db ->
+let make_persistent_lazy_lwt ~store ~name ~default = use_pool @@ fun db ->
   let query = sprintf "SELECT value FROM %s WHERE key = $1 " store in
   lwt result = exec db query [name] in
   lwt _ = begin match result with
@@ -102,25 +90,25 @@ let make_persistent_lazy ~store ~name ~default =
 let make_persistent ~store ~name ~default =
   make_persistent_lazy ~store ~name ~default:(fun () -> default)
 
-let get p = full_transaction_block @@ fun db ->
+let get p = use_pool @@ fun db ->
   let query = sprintf "SELECT value FROM %s WHERE key = $1 " p.store in
   Lwt.map (unmarshal @. one) (exec db query [p.name])
 
-let set p v = full_transaction_block @@ fun db ->
+let set p v = use_pool @@ fun db ->
   insert db p.store p.name v
 
 type 'value table = string
 
 let table_name table = Lwt.return table
 
-let open_table table = full_transaction_block @@ fun db ->
+let open_table table = use_pool @@ fun db ->
   create_table db table >> Lwt.return table
 
-let find table key = full_transaction_block @@ fun db ->
+let find table key = use_pool @@ fun db ->
   let query = sprintf "SELECT value FROM %s WHERE key = $1 " table in
   Lwt.map (unmarshal @. one) (exec db query [key])
 
-let add table key value = full_transaction_block @@ fun db ->
+let add table key value = use_pool @@ fun db ->
   insert db table key value
 
 let replace_if_exists table key value =
@@ -128,15 +116,15 @@ let replace_if_exists table key value =
     find table key >> add table key value
   with Not_found -> Lwt.return ()
 
-let remove table key = full_transaction_block @@ fun db ->
+let remove table key = use_pool @@ fun db ->
   let query = sprintf "DELETE FROM %s WHERE key = $1 " table in
   exec db query [key] >> Lwt.return ()
 
-let length table = full_transaction_block @@ fun db ->
+let length table = use_pool @@ fun db ->
   let query = sprintf "SELECT count(*) FROM %s " table in
   Lwt.map (unmarshal @. one) (exec db query [])
 
-let iter_step f table = full_transaction_block @@ fun db ->
+let iter_step f table = use_pool @@ fun db ->
   let query = sprintf "SELECT * FROM %s " table in
   PGOCaml.prepare db ~query () >>
   PGOCaml.cursor db ~params:[] @@
