a62c1483c04337db4594c70239b21995394ed2fb
Vincent Balat <vincent.balat@vblt.org>
2024-04-27 19:27:42 +0100

    Static linking: using new instruction system for rewritemod

---
+++ src/extensions/rewritemod.mli
@@ -0,0 +1,10 @@
+val section : Lwt_log_core.section
+(** use Lwt_log.Section.set_level in order to debug *)
+
+val run :
+   ?continue:bool
+  -> ?full_rewrite:bool
+  -> regexp:string
+  -> string
+  -> unit
+  -> Ocsigen_server.Site.instruction
