aed104f894ed65805fa4f40214fe067e9e84f7dc
Vincent Balat <vincent.balat@vblt.org>
2024-04-27 18:21:40 +0100

    Static linking: using new instruction system for deflatemod

--- src/extensions/deflatemod.mli
+++ src/extensions/deflatemod.mli
@@ -1,13 +1,12 @@
 val section : Lwt_log_core.section
 (** use Lwt_log.Section.set_level in order to debug *)
 
-val compress_level : int Ocsigen_config.Custom.key
-val buffer_size : int Ocsigen_config.Custom.key
+val set_compress_level : int -> unit
+val set_buffer_size : int -> unit
 
 type filter = [`Type of string option * string option | `Extension of string]
 
-val mode :
-  [`All_but of filter list | `Only of filter list]
-    Ocsigen_server.Site.Config.key
-
-val extension : Ocsigen_server.Site.instruction
+val run :
+   mode:[`All_but of filter list | `Only of filter list]
+  -> unit
+  -> Ocsigen_server.Site.instruction
