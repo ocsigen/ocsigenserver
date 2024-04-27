e671e30ed3477f8e4cc12a7290d2912d20a24f22
Vincent Balat <vincent.balat@vblt.org>
2024-04-27 19:15:44 +0100

    Static linking: using new instruction system for revproxy

---
+++ src/extensions/revproxy.mli
@@ -0,0 +1,14 @@
+val section : Lwt_log_core.section
+(** use Lwt_log.Section.set_level in order to debug *)
+
+type redirection
+
+val create_redirection :
+   ?full_url:[< `Maybe | `No | `Yes > `Yes]
+  -> ?pipeline:bool
+  -> ?keephost:bool
+  -> regexp:string
+  -> string
+  -> redirection
+
+val run : redirection:redirection -> unit -> Ocsigen_server.Site.instruction
