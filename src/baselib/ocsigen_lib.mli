0ee6e69fb7888ff57d94df2d35ff0e57fb6a207e
Vincent Balat <vincent.balat@vblt.org>
2026-01-16 14:43:03 +0100

    Removing dependency to eio_unix for baselib.base

--- src/baselib/ocsigen_lib.mli
+++ src/baselib/ocsigen_lib.mli
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-*)
+ *)
 
 (** This module contains some auxiliaries for the Ocsigenserver. In contrast to
     {!Ocsigen_lib_base}, the function may also refer to libraries other than the
